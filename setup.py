e3d6de80eb0d93021bbb281b81660977fe08cc76
phimi <michel@apiomat.com>
2013-04-29 12:25:44 +0200

    added URL

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ from setuptools import setup, find_packages
 setup(
     name = "import-parse-to-apiOmat",
     description = "Import your data from Parse to apiOmat",
+    url = "https://github.com/apiOmat/apiOmat-import-parse",
     version = "1.0",
     scripts=['ImportParseData/import-parse-to-apiOmat'],
     packages = find_packages(),
